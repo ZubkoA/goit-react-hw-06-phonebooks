aa262359c11028188a3d2db3a3ec5cbdd769cb98
ZubkoA <114995045+ZubkoA@users.noreply.github.com>
2023-06-11 16:15:22 +0300

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,45 +1,16 @@
 import Header from './Header';
-import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useState } from 'react';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
 import AddContacts from './AddContacts/AddContacts';
-// import { nanoid } from 'nanoid';
 import css from './App.module.css';
 
-import React from 'react';
-
 const App = () => {
-  const [contacts, setContacts] = useState([]);
-  const [filter, setFilter] = useState('');
-
-  const handleChange = e => {
-    setFilter(e.target.value);
-  };
-
-  const addContact = (name, number) => {
-    contacts.some(contact => contact.name === name)
-      ? alert(`${name} is already in contacts.`)
-      : setContacts(prev => [
-          ...prev,
-          {
-            name,
-            number,
-          },
-        ]);
-  };
-  // const findFilter = () => {
-  //   const normalizeFilter = filter.toLowerCase();
-  //   return contacts.filter(contact =>
-  //     contact.name.toLowerCase().includes(normalizeFilter)
-  //   );
-  // };
-
   return (
     <div className={css.container}>
       <Header title="Phonebook" />
-      <AddContacts addContact={addContact} />
-      <Filter value={filter} onChange={handleChange} />
+      <AddContacts />
+      <Filter />
       <Header titleContacts="Contacts" />
       <ContactList />
     </div>
